6ab822b500f8661444dc62c80ff506b3cf7dcbd2
Aelie54 <klein.amelie1991@gmail.com>
2022-03-25 09:26:37 +0100

    ok registration ok login

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('sandbox@loryleticee.com', 'Amélie site Training'))
+                    ->from(new Address('alajitest5@pawolanmwen.com', 'Amélie site Training'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
